2ab9e7d164b1a316fc6deae5d6abbb5a2c9278da
muxufo <crelier.nicolas@gmail.com>
2016-04-06 23:26:01 +0200

    Controlleur

--- App/Controller/PanierController.php
+++ App/Controller/PanierController.php
@@ -1,7 +1,48 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: PC-Nicolas
- * Date: 06/04/2016
- * Time: 23:15
- */
+namespace App\Controller;
+
+use Silex\Application;
+use Silex\ControllerProviderInterface;
+
+use Symfony\Component\HttpFoundation\Request;   // pour utiliser request
+
+use App\Model\ProduitModel;
+use App\Model\TypeProduitModel;
+
+use Symfony\Component\Validator\Constraints as Assert;   // pour utiliser la validation
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Security;
+
+class PanierController implements ControllerProviderInterface
+{
+    private $panierModel;
+    private $typePanierModel;
+
+    public function __construct()
+    {
+    }
+
+    public function index(Application $app) {
+        return $this->show($app);
+    }
+
+    public function show(Application $app) {
+        $this->panierModel = new PanierModel($app);
+        $panier = $this->panierModel->getAllPanier();
+        return $app["twig"]->render('backOff/Panier/show.html.twig',['data'=>$panier]);
+    }
+
+    public function connect(Application $app) {  //http://silex.sensiolabs.org/doc/providers.html#controller-providers
+        $controllers = $app['controllers_factory'];
+
+        $controllers->get('/', 'App\Controller\panierController::index')->bind('panier.index');
+        $controllers->get('/show', 'App\Controller\panierController::show')->bind('panier.show');
+
+
+
+        return $controllers;
+    }
+
+
+}
