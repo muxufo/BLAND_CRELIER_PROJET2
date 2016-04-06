eaabfd5c6b12ad0c232af3e8d18eec93a9be5c05
gaetan <gaetan.bland@edu.univ-fcomte.fr>
2016-04-06 23:36:33 +0200

    Ajout de la méthode getAllPanier dans le modèle

--- App/Model/PanierModel.php
+++ App/Model/PanierModel.php
@@ -1,8 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: PC-Nicolas
- * Date: 06/04/2016
- * Time: 23:14
- */
+namespace App\Model;
 
+use Doctrine\DBAL\Query\QueryBuilder;
+use Silex\Application;
+
+class PanierModel {
+    private $db;
+
+    public function __construct(Application $app) {
+        $this->db = $app['db'];
+    }
+
+    public function getAllPanier() {
+        $queryBuilder = new QueryBuilder($this->db);
+        $queryBuilder
+            ->select('*')
+            ->from('paniers', 'pan')
+            ->innerJoin('pan', 'produit', 'p', 'pan.id_produit = p.id_produit')
+            ->addOrderBy('pan.id_produit', 'ASC');
+        return $queryBuilder->execute()->fetchAll();
+    }
+}
