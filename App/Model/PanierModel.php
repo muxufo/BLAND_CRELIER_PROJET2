42f3a8b65be149d345cf19e8134cfae2003ec0b8
gaetan <gaetan.bland@edu.univ-fcomte.fr>
2016-05-12 18:17:54 +0200

    Modif de la méthode updateLigneAdd et de la "propreté" des autres méthodes de la classe

--- App/Model/PanierModel.php
+++ App/Model/PanierModel.php
@@ -14,37 +14,42 @@ class PanierModel {
 
     public function getAllPanier() {
         $queryBuilder = new QueryBuilder($this->db);
-        $queryBuilder
-            ->select('pa.produit_id,pa.id,pr.nom,pa.quantite,pa.prix,pa.dateAjoutPanier')
-            ->from('paniers pa', ',produits pr' )
-            ->where('pr.id=produit_id')
-            ->orderBy('pr.nom', 'ASC');
+        $queryBuilder->select('pa.produit_id,pa.id,pr.nom,pa.quantite,pa.prix,pa.dateAjoutPanier')
+                     ->from('paniers pa', ',produits pr' )
+                     ->where('pr.id=produit_id')
+                     ->orderBy('pr.nom', 'ASC');
         return $queryBuilder->execute()->fetchAll();
     }
 
 
 
-    function counNbProduitLigne($produit_id,$user_id){
+    function counNbProduitLigne($produit_id, $user_id){
         $queryBuilder = new QueryBuilder($this->db);
 
         $queryBuilder
             ->select('count(produit_id')->from('paniers')
             ->where('produit_id= :idProduit')->andWhere('user_id = :id_User')
             ->andWhere('commande_id is Null')
-            ->setParameter('idProduit',$produit_id)->setParameter('user_id',$user_id);
+            ->setParameter('idProduit', $produit_id)->setParameter('user_id', $user_id);
         return $queryBuilder ->execute()->fetchColumn(0);
     }
 
-    function updateLigneAdd($produit_id,$user_id){
-
+    function updateLigneAdd($quantite, $prix, $user_id, $produit_id){
+        $queryBuilder = new QueryBuilder($this->db);
+        $queryBuilder->update('paniers')
+                     ->set('quantite', 'quantite + :quantite')
+                     ->set('prix', 'prix + :prix * :quantite')
+                     ->set('dateAjoutPanier', 'CURRENT_TIMESTAMP')
+                     ->where('produit_id = :idProduit')
+                     ->setParameter('quantite', $quantite)->setParameter('prix', $prix)->setParameter('idUser', $user_id)->setParameter('idProduit', $produit_id);
+        $queryBuilder->execute();
     }
 
     function deleteArticle($produit_id, $user_id) {
         $queryBuilder = new QueryBuilder($this->db);
-        $queryBuilder
-            ->delete('paniers')
-            ->setParameter('produit_id',$produit_id)
-            ->setParameter('user_id',$user_id)
+        $queryBuilder->delete('paniers')
+                     ->setParameter('produit_id',$produit_id)
+                     ->setParameter('user_id',$user_id)
         ;
         return $queryBuilder->execute();
     }
@@ -52,8 +57,8 @@ class PanierModel {
     function inserLigne($quantite, $prix, $user_id, $produit_id){
         $queryBuilder = new QueryBuilder($this->db);
         $queryBuilder->insert('paniers')
-            ->values(['$quantite' => ':quantite', '$prix' =>':prix', '$user_id' => ':idUser', 'produit_id' => ':idProduit'])
-            ->setParameter('quantite', $quantite)->setParameter('prix', $prix)->setParameter('idUser', $user_id)->setParameter('idProduit', $produit_id);
+                     ->values(['$quantite' => ':quantite', '$prix' =>':prix', '$user_id' => ':idUser', 'produit_id' => ':idProduit'])
+                     ->setParameter('quantite', $quantite)->setParameter('prix', $prix)->setParameter('idUser', $user_id)->setParameter('idProduit', $produit_id);
         return $queryBuilder->execute();
     }
 }
